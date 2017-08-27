3fa07d5fa8a1065aaca1cca44cd34edc885035fd
Louis Lagrange <lagrange.louis@gmail.com>
2017-08-27 18:40:58 +0200

    Add component from DailyScrum

---
+++ src/index.js
@@ -0,0 +1,92 @@
+// @flow
+import React, { Component } from 'react';
+import { View, Animated, Platform, StyleSheet } from 'react-native';
+import Animation from 'lottie-react-native';
+
+const linearEasing = t => t;
+
+export default class LottieAnimation extends Component<void, Props, State> {
+  state = {
+    progress: new Animated.Value(0),
+  };
+
+  shouldComponentUpdate(nextProps: Props) {
+    return this.props.source.nm !== nextProps.source.nm;
+  }
+
+  componentDidMount() {
+    this.animate();
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (this.props.source.nm !== prevProps.source.nm) {
+      this.animate();
+    }
+  }
+
+  animate = () => {
+    const animation = this.props.animation ? this.props.animation(this.state.progress) : this.getDefaultAnimation();
+    this.state.progress.setValue(0);
+    animation.start(this.props.loop ? this.animate : null);
+  };
+
+  getDefaultAnimation = () =>
+    Animated.timing(this.state.progress, {
+      toValue: 1,
+      delay: this.props.delay,
+      easing: this.props.easing ? this.props.easing : linearEasing,
+      duration: this.props.duration || Math.round(this.props.source.op / this.props.source.fr * 1000),
+      useNativeDriver: true,
+    });
+
+  render() {
+    const { style, source, ...rest } = this.props;
+    const aspectRatio = source.w / source.h;
+    const styleObject = StyleSheet.flatten(style);
+    return (
+      <View
+        style={[
+          {
+            aspectRatio,
+          },
+          (!styleObject || (styleObject.width === undefined && styleObject.height === undefined)) && {
+            width: source.w,
+          },
+          style,
+        ]}
+      >
+        <Animation
+          progress={this.state.progress}
+          style={{
+            aspectRatio,
+            width: '100%',
+          }}
+          source={Platform.select({
+            ios: source,
+            android: {
+              ...source,
+              fr: Math.round(source.fr),
+              ip: Math.round(source.ip),
+              op: Math.round(source.op),
+            },
+          })}
+          {...rest}
+        />
+      </View>
+    );
+  }
+}
+
+type State = {
+  progress: Animated.Value,
+};
+
+type Props = {
+  style?: any,
+  source: Object, // require('path/to/animation.json')
+  animation?: Animated.Value => Object, // Animated.Value => CompositeAnimation
+  loop?: boolean,
+  duration?: number,
+  delay?: number,
+  easing?: Function,
+};
